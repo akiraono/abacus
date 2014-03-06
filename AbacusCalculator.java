e98f92aa53544afd37b0d8bfc738c64b1d338c9f
ono <ono@ono-CF-SX2JETDR.(none)>
2014-03-06 15:21:03 +0900

    update one line

--- AbacusCalculator.java
+++ AbacusCalculator.java
@@ -1,5 +1,9 @@
 public class AbacusCalculator {
     public static void main(String[] args){
+	AbacusCalculator app = new AbacusCalculator();
+	app.processMain();
+    }
+    private void processMain(){
 	System.out.println("Abacus Calculator");
     }
 }
